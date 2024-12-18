2f38ef5997893408e907e3eebc287e4078eb7ce8
Tony <tony.xvr@gmail.com>
2024-12-18 12:08:22 +0300

    V21 update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css';
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from '@vercel/analytics/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { ProfileProvider } from '@/contexts/ProfileContext';
@@ -126,6 +127,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className="min-h-screen">
         <Component {...pageProps} />
         <Toaster />
+        <Analytics />
       </div>
     </ProfileProvider>
   )
