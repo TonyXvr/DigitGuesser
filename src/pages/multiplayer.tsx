ca6e66e56fc2ff77918129540ec7f01caf2160e7
Tony <tony.xvr@gmail.com>
2024-12-24 19:23:30 +0300

    Update multiplayer.tsx

--- src/pages/multiplayer.tsx
+++ src/pages/multiplayer.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { useState } from "react"
